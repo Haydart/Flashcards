4642a3150992e1cde881a7ab6df099e01a920cec
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-12 21:14:50 +0200

    feat(domain): add GetCategories, GetSubcategories, and GetFlashcards use cases

---
+++ app/src/main/java/com/rossomak/flashcards/domain/usecase/GetFlashcardsUseCase.kt
@@ -0,0 +1,13 @@
+package com.rossomak.flashcards.domain.usecase
+
+import com.rossomak.flashcards.domain.model.Flashcard
+import com.rossomak.flashcards.domain.repository.FlashcardRepository
+import com.rossomak.flashcards.domain.usecase.base.UseCase
+import javax.inject.Inject
+
+class GetFlashcardsUseCase @Inject constructor(
+    private val repository: FlashcardRepository
+) : UseCase<String, Result<List<Flashcard>>> {
+    override suspend operator fun invoke(params: String): Result<List<Flashcard>> =
+        repository.fetchFlashcards(params)
+}
---
+++ app/src/main/java/com/rossomak/flashcards/domain/usecase/GetSubcategoriesUseCase.kt
@@ -0,0 +1,13 @@
+package com.rossomak.flashcards.domain.usecase
+
+import com.rossomak.flashcards.domain.model.Subcategory
+import com.rossomak.flashcards.domain.repository.FlashcardRepository
+import com.rossomak.flashcards.domain.usecase.base.UseCase
+import javax.inject.Inject
+
+class GetSubcategoriesUseCase @Inject constructor(
+    private val repository: FlashcardRepository
+) : UseCase<String, Result<List<Subcategory>>> {
+    override suspend operator fun invoke(params: String): Result<List<Subcategory>> =
+        repository.fetchSubcategories(params)
+}
