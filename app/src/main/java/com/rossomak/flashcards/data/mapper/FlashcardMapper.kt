5a263bc0f38c763940a89f1b281c397dc5c013ed
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-12 16:39:51 +0200

    feat(data): add toDomain() mapper extensions for all three DTOs

---
+++ app/src/main/java/com/rossomak/flashcards/data/mapper/FlashcardMapper.kt
@@ -0,0 +1,37 @@
+package com.rossomak.flashcards.data.mapper
+
+import com.rossomak.flashcards.data.model.CategoryDto
+import com.rossomak.flashcards.data.model.FlashcardDto
+import com.rossomak.flashcards.data.model.SubcategoryDto
+import com.rossomak.flashcards.domain.model.Category
+import com.rossomak.flashcards.domain.model.Flashcard
+import com.rossomak.flashcards.domain.model.Subcategory
+
+fun CategoryDto.toDomain() = Category(
+    id = id,
+    name = name,
+    order = order,
+    subcategoryCount = subcategoryCount,
+    iconUrl = iconUrl
+)
+
+fun SubcategoryDto.toDomain() = Subcategory(
+    id = id,
+    name = name,
+    categoryId = categoryId,
+    order = order,
+    cardCount = cardCount
+)
+
+fun FlashcardDto.toDomain(subcategoryId: String) = Flashcard(
+    id = id,
+    subcategoryId = subcategoryId,
+    question = question,
+    answer = answer,
+    tags = tags,
+    questionCode = questionCode,
+    answerCode = answerCode,
+    questionSpoken = questionSpoken,
+    answerSpoken = answerSpoken,
+    extendedContext = extendedContext
+)
