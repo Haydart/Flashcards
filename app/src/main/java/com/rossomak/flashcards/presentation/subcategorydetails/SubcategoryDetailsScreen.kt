cfc5e647144e7cc05fc77f7eb7c9cbb87c16bc08
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-14 13:25:09 +0200

    feat(subcategorydetails): add SubcategoryDetailsScreen composable scaffold

--- app/src/main/java/com/rossomak/flashcards/presentation/subcategorydetails/SubcategoryDetailsScreen.kt
+++ app/src/main/java/com/rossomak/flashcards/presentation/subcategorydetails/SubcategoryDetailsScreen.kt
@@ -1,4 +1,23 @@
 package com.rossomak.flashcards.presentation.subcategorydetails
 
-class SubcategoryDetailsScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun SubcategoryDetailsScreen(
+    onNavigateBack: () -> Unit,
+    viewModel: SubcategoryDetailsViewModel = hiltViewModel(),
+) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Subcategory Details Screen, NYI")
+    }
 }
