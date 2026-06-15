07bac71f8c7cd0771f1c56b1d6b674c0c82ee8b5
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-15 12:54:14 +0200

    refactor: add subcategory details destination in Home screen

--- app/src/main/java/com/rossomak/flashcards/presentation/home/HomeDestination.kt
+++ app/src/main/java/com/rossomak/flashcards/presentation/home/HomeDestination.kt
@@ -4,4 +4,6 @@ import com.rossomak.flashcards.ui.navigation.NavigationDestination
 
 sealed interface HomeDestination : NavigationDestination {
     data class CategoryDetails(val categoryId: String, val categoryName: String) : HomeDestination
+
+    data class SubcategoryDetails(val subcategoryId: String, val subcategoryName: String, val categoryId: String, val categoryName: String) : HomeDestination
 }
