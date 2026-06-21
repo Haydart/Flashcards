e30cf7dc03fb22d4a98e705ee218f4beca4963ca
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-21 19:07:53 +0200

    feat(di): bind VoiceGateway in Hilt VoiceModule

---
+++ app/src/main/java/com/rossomak/flashcards/di/VoiceModule.kt
@@ -0,0 +1,18 @@
+package com.rossomak.flashcards.di
+
+import com.rossomak.flashcards.domain.voice.VoiceGateway
+import com.rossomak.flashcards.service.StudySessionVoiceGateway
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class VoiceModule {
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindVoiceGateway(impl: StudySessionVoiceGateway): VoiceGateway
+}
