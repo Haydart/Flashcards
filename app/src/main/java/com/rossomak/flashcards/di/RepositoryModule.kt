91c5ccdf7e2068bd8a49a9257bc2fbf9cd7db345
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-23 10:56:48 +0200

    chore(di): bind CurationRepository to CurationRepositoryImpl in RepositoryModule

--- app/src/main/java/com/rossomak/flashcards/di/RepositoryModule.kt
+++ app/src/main/java/com/rossomak/flashcards/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.rossomak.flashcards.di
 
 import com.rossomak.flashcards.data.repository.AuthRepositoryImpl
+import com.rossomak.flashcards.data.repository.CurationRepositoryImpl
 import com.rossomak.flashcards.data.repository.FlashcardRepositoryImpl
 import com.rossomak.flashcards.domain.repository.AuthRepository
+import com.rossomak.flashcards.domain.repository.CurationRepository
 import com.rossomak.flashcards.domain.repository.FlashcardRepository
 import dagger.Binds
 import dagger.Module
@@ -21,4 +23,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindFlashcardRepository(flashcardRepositoryImpl: FlashcardRepositoryImpl): FlashcardRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCurationRepository(curationRepositoryImpl: CurationRepositoryImpl): CurationRepository
 }
