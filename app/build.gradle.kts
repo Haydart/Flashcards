8a1342fb63c426a83c97998effbe1b5dcf532ec4
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-28 20:27:11 +0200

    chore(app): add :feature:study dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(project(":feature:settings"))
     implementation(project(":feature:home"))
     implementation(project(":feature:browse"))
+    implementation(project(":feature:study"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
