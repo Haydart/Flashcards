27f04df8b6a32da48a52ea372ef5f80791d5f37e
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-28 21:07:15 +0200

    refactor(app): remove login presentation sources migrated to :feature:auth

--- app/src/main/java/com/rossomak/flashcards/presentation/login/LoginDestination.kt
+++
@@ -1,7 +0,0 @@
-package com.rossomak.flashcards.presentation.login
-
-import com.rossomak.flashcards.ui.navigation.NavigationDestination
-
-sealed interface LoginDestination : NavigationDestination {
-    data object Main : LoginDestination
-}
--- app/src/main/java/com/rossomak/flashcards/presentation/login/LoginScreenState.kt
+++
@@ -1,7 +0,0 @@
-package com.rossomak.flashcards.presentation.login
-
-data class LoginScreenState(
-    val isSigningIn: Boolean = false,
-    val errorMessage: String? = null,
-    val navigationDestination: LoginDestination? = null
-)
