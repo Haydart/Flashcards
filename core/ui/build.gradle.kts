06bebe99c47f1697cebe290e8a751b24c6fd46b8
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-06-29 12:09:13 +0200

    feat(core:ui): absorb theme system from :core:design

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -9,11 +9,11 @@ android {
 dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
-    api(project(":core:design"))
     implementation(project(":core:domain"))
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.syntaxmp)
 }
