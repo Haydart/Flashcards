d7399cd19d2d42dc466394e40a5c6a1650aef62a
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-07-04 00:29:07 +0200

    fix(domain): make StudyMode serializable for route navigation

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.javax.inject)
     testFixturesImplementation(libs.junit)
     testFixturesImplementation(libs.kotlinx.coroutines.test)
--- core/domain/src/main/kotlin/com/rossomak/flashcards/core/domain/model/StudyMode.kt
+++ core/domain/src/main/kotlin/com/rossomak/flashcards/core/domain/model/StudyMode.kt
@@ -1,5 +1,8 @@
 package com.rossomak.flashcards.core.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class StudyMode {
     RATED,
     FAST,
