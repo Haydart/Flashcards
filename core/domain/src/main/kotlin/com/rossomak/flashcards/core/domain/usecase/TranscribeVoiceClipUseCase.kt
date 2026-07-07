48bb3cfdd6e6c70ed4d7498bd1ba128edfebf2b3
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-07-07 13:44:16 +0200

    feat(domain): add grading/transcription use cases

---
+++ core/domain/src/main/kotlin/com/rossomak/flashcards/core/domain/usecase/TranscribeVoiceClipUseCase.kt
@@ -0,0 +1,13 @@
+package com.rossomak.flashcards.core.domain.usecase
+
+import com.rossomak.flashcards.core.domain.repository.VoiceAnswerGradingRepository
+import com.rossomak.flashcards.core.domain.usecase.base.UseCase
+import javax.inject.Inject
+
+class TranscribeVoiceClipUseCase @Inject constructor(
+    private val voiceAnswerGradingRepository: VoiceAnswerGradingRepository,
+) : UseCase<ByteArray, Result<String>> {
+
+    override suspend fun invoke(params: ByteArray): Result<String> =
+        voiceAnswerGradingRepository.transcribe(params)
+}
