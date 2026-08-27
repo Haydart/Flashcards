1aad5869150c0ef89858c40caeeede1119440b80
rmakowiecki <makowiecki.radoslaw@gmail.com>
2026-08-27 16:19:44 +0200

    chore: delete obsolete voice settings and consent repositories

--- core/domain/src/main/kotlin/com/rossomak/flashcards/core/domain/repository/VoiceSettingsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.rossomak.flashcards.core.domain.repository
-
-import com.rossomak.flashcards.core.domain.model.VoiceSettings
-import kotlinx.coroutines.flow.Flow
-
-interface VoiceSettingsRepository {
-    fun voiceSettings(): Flow<VoiceSettings>
-    suspend fun save(settings: VoiceSettings)
-}
